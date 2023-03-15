eb68578863d95604664eea5e33509608827190a3
Abu Sayeed <sayeed@pop-os.localdomain>
2023-03-15 20:31:00 +0600

    added a form in the banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -12,7 +12,12 @@ const Banner = () => {
                             <p className='mb-4'>We have 850,000 great job offers you deserve!</p>
                             <h1 className="text-5xl font-bold">Find The Most Exciting</h1>
                             <h1 className="text-5xl font-bold mt-2">Startup Jobs</h1>
-                            <button className="btn btn-primary">Get Started</button>
+                            <form className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 mt-8 p-0 lg:p-5 shadow-xl rounded-xl'>
+                                <input type="text" placeholder="Job Title" className="input w-full max-w-xs" />
+                                <input type="text" placeholder="Category" className="input w-full max-w-xs" />
+                                <input type="text" placeholder="Location" className="input w-full max-w-xs" />
+                                <input className="btn btn-primary bg-gradient-to-r from-[#0FCFEC] to-[#19D3AE] text-white font-bold border-none" value='Search' type="text" />
+                            </form>
                         </div>
                     </div>
                 </div>

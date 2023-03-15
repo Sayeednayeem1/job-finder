ec00f1752c6e6db2a13840cf17997c8e6422c4f5
Abu Sayeed <sayeed@pop-os.localdomain>
2023-03-15 11:16:25 +0600

    installed react router dom

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>This is a test</h1>
+      <button className='btn btn-primary'>test</button>
     </div>
   );
 }

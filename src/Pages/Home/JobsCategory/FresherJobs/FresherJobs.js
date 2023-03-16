56d0f953cece6485f30cdf9f0c5a9ad4ba2b9d9a
Abu Sayeed <sayeed@pop-os.localdomain>
2023-03-16 11:04:46 +0600

    added FresherJobs related logo svg and updated the fake data

--- src/Pages/Home/JobsCategory/FresherJobs/FresherJobs.js
+++ src/Pages/Home/JobsCategory/FresherJobs/FresherJobs.js
@@ -1,49 +1,56 @@
 import React from 'react';
+import FresherJob from './FresherJob';
+import Dev1 from '../../../../assets/Images/Fresher-Job-Assets/Dev1.svg';
+import Dev2 from '../../../../assets/Images/Fresher-Job-Assets/Dev2.svg';
+import Dev3 from '../../../../assets/Images/Fresher-Job-Assets/Dev3.svg';
+import Dev4 from '../../../../assets/Images/Fresher-Job-Assets/Dev4.svg';
+import Dev5 from '../../../../assets/Images/Fresher-Job-Assets/Dev5.svg';
+import Dev6 from '../../../../assets/Images/Fresher-Job-Assets/Dev6.svg';
 
 const FresherJobs = () => {
 
-    const fresherJobData = [
+    const fresherJobsData = [
         {
             id: 1,
             name: "Front-End Development",
             location: "Dhaka",
             companyName: "IT Farm Tech",
-            logo: "",
+            logo: Dev1
         },
         {
             id: 2,
             name: "Back-End Development",
             location: "Chittagong",
             companyName: "IT Farm Tech",
-            logo: "",
+            logo: Dev2,
         },
         {
             id: 3,
             name: "React.js Development",
             location: "Sylhet",
             companyName: "IT Farm Tech",
-            logo: "",
+            logo: Dev3,
         },
         {
             id: 4,
             name: "FullStacK Development",
             location: "Barisal",
             companyName: "IT Farm Tech",
-            logo: "",
+            logo: Dev4,
         },
         {
             id: 5,
             name: "Node.js Development",
             location: "Rajshahi",
             companyName: "IT Farm Tech",
-            logo: "",
+            logo: Dev5,
         },
         {
             id: 6,
             name: "Next.js Development",
             location: "Khulna",
             companyName: "IT Farm Tech",
-            logo: "",
+            logo: Dev6
         }
     ]
 
@@ -55,7 +62,12 @@ const FresherJobs = () => {
                     <p className='text-[#176CCF] font-bold'>Browse 200+ Top Jobs For Freshers</p>
                 </div>
                 <div>
-
+                    {
+                        fresherJobsData.map(data => <FresherJob
+                            key={data.id}
+                            data={data}
+                        ></FresherJob>)
+                    }
                 </div>
             </div>
         </div>

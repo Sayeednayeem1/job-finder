6f5a9d9fa3dd1c882097b3458ac2eca6d1e79367
Abu Sayeed <sayeed@pop-os.localdomain>
2023-03-16 19:24:02 +0600

    added assets for WhyAreDifferent component

--- src/Pages/Home/WhyAreWeDifferent/WhyAreWeDifferent.js
+++ src/Pages/Home/WhyAreWeDifferent/WhyAreWeDifferent.js
@@ -3,7 +3,35 @@ import React from 'react';
 const WhyAreWeDifferent = () => {
     return (
         <div>
-            <h1>This is about me being different</h1>
+            <div>
+                <h1>Why we are different</h1>
+                <p>We collect reviews from our users so you can get an honest opinion of what an experience with our website.</p>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <figure>
+                            <img src="" alt="" />
+                        </figure>
+                        <p>Keep your cool. Get on-demand help to on-demand make the best offer.</p>
+                    </div>
+                    <div>
+                        <figure>
+                            <img src="" alt="" />
+                        </figure>
+                        <p>Keep your cool. Get on-demand help to on-demand make the best offer.</p>
+                    </div>
+                    <div>
+                        <figure>
+                            <img src="" alt="" />
+                        </figure>
+                        <p>Keep your cool. Get on-demand help to on-demand make the best offer.</p>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     );
 };

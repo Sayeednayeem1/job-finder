ef72a2e5884fe742ed703db4342592e1f6e8fbc7
Abu Sayeed <sayeed@pop-os.localdomain>
2023-03-16 12:38:27 +0600

    added padding, mt for smallaer devices for BuildJobBoard Component

--- src/Pages/Home/BuildJobBoard/BuildJobBoard.js
+++ src/Pages/Home/BuildJobBoard/BuildJobBoard.js
@@ -3,7 +3,10 @@ import React from 'react';
 const BuildJobBoard = () => {
     return (
         <div>
-            <h1>this is a test</h1>
+            <div className='bg-blue-600 flex flex-col md:flex-row items-center justify-evenly p-16'>
+                <h1 className='text-4xl text-white font-bold text-center'>Start building your own job board now!</h1>
+                <button class="btn border text-white mt-4 md:mt-0 bg-blue-600 border-white font-bold hover:bg-white hover:text-blue-500">Search Now</button>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import BuildJobBoard from '../BuildJobBoard/BuildJobBoard';
 import ExperiencedJobs from '../JobsCategory/ExperiencedJobs/ExperiencedJobs';
 import FresherJobs from '../JobsCategory/FresherJobs/FresherJobs';
 
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Banner />
             <FresherJobs />
+            <BuildJobBoard />
             <ExperiencedJobs></ExperiencedJobs>
         </div>
     );

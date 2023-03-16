978b67fed41efe9735439ebcf70164e29bafe72c
Abu Sayeed <sayeed@pop-os.localdomain>
2023-03-16 12:16:25 +0600

    created BuildJobBoard Component and added it in the home route

---
+++ src/Pages/Home/BuildJobBoard/BuildJobBoard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BuildJobBoard = () => {
+    return (
+        <div>
+            <h1>this is a test</h1>
+        </div>
+    );
+};
+
+export default BuildJobBoard;
